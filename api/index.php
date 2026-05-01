f8bc931a80d5b1b9cd6b8f365409b48bf9948183
mario-leandro <marioleandro2004@gmail.com>
2026-05-01 16:18:27 -0300

    a

--- api/index.php
+++ api/index.php
@@ -4,41 +4,49 @@ require_once __DIR__ . "/../config/common.php";
 
 headers();
 
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    http_response_code(405);
+    echo json_encode(["error" => "Método não permitido"]);
+    logMsg("Método não permitido: " . $_SERVER['REQUEST_METHOD'] . " em " . $_SERVER['REQUEST_URI']);
+    exit;
+}
 $dados = json_decode(file_get_contents("php://input"), true);
 
-if (!$dados) {
-    http_response_code(400);
-    echo json_encode([
-        "success" => false,
-        "message" => "Body inválido ou vazio"
-    ]);
-    exit;
-}
+// if (!$dados) {
+//     http_response_code(400);
+//     echo json_encode([
+//         "success" => false,
+//         "message" => "Body inválido ou vazio"
+//     ]);
+//     exit;
+// }
 
-$type = $dados["type"] ?? null;
-$action = $dados["action"] ?? null;
-$data = $dados["data"] ?? [];
+var_dump($dados);
 
-if (!$type || !$action) {
-    http_response_code(400);
-    echo json_encode([
-        "success" => false,
-        "message" => "Informe type e action"
-    ]);
-    exit;
-}
+// $type = $dados["type"] ?? null;
+// $action = $dados["action"] ?? null;
+// $data = $dados["data"] ?? [];
 
-global $routes;
+// if (!$type || !$action) {
+//     http_response_code(400);
+//     echo json_encode([
+//         "success" => false,
+//         "message" => "Informe type e action"
+//     ]);
+//     exit;
+// }
 
-if (!isset($routes[$type][$action])) {
-    http_response_code(404);
-    echo json_encode([
-        "success" => false,
-        "message" => "Rota não encontrada"
-    ]);
-    exit;
-}
+// global $routes;
 
-$_REQUEST_DATA = $data;
+// if (!isset($routes[$type][$action])) {
+//     http_response_code(404);
+//     echo json_encode([
+//         "success" => false,
+//         "message" => "Rota não encontrada"
+//     ]);
+//     exit;
+// }
 
-require_once $routes[$type][$action];
+// $_REQUEST_DATA = $data;
+
+// require_once $routes[$type][$action];
