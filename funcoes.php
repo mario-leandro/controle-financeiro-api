3a392bf31d0c09caa69f53eddc70c317902b2478
mario-leandro <marioleandro2004@gmail.com>
2026-05-07 10:36:21 -0300

    ajustando cors

--- funcoes.php
+++ funcoes.php
@@ -15,7 +15,7 @@ function headers()
     }
 
     header("Access-Control-Allow-Credentials: true");
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); // adicionar put e delete quando os implementar
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
     header("Content-Type: application/json");
 
