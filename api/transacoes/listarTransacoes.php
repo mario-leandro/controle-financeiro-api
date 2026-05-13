a45224af66d430c6dd9920717e9ea139c8f985c4
mario-leandro <marioleandro2004@gmail.com>
2026-05-13 11:39:43 -0300

    adicionando novo metodo no database - fazendo inner join para pegar o nome da categoria pelo category_id

--- api/transacoes/listarTransacoes.php
+++ api/transacoes/listarTransacoes.php
@@ -14,7 +14,7 @@ $db_connection = null;
 try {
     $db_connection = new Database();
 
-    $transacoes = $db_connection->sql("SELECT t.*, c.nome FROM cf_db.transactions t INNER JOIN cf_db.categories c ON t.category_id = c.id WHERE t.usuario_id = " . $usuarioId);
+    $transacoes = $db_connection->sql("SELECT t.*, c.nome as categoria_nome FROM cf_db.transactions t INNER JOIN cf_db.categories c ON t.category_id = c.id WHERE t.usuario_id = " . $usuarioId);
 
     echo json_encode([
         "success" => true,
